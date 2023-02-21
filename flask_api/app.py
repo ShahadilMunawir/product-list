3a038a011588e403b0c339459c2d92cba300baa5
Kethankrk <kethankrk@gmail.com>
2023-02-22 03:39:16 +0530

    api

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, send_file
 from flask_bcrypt import Bcrypt
 from flask_session import Session
 from flask_cors import CORS, cross_origin
-from model import db, User
+from model import db, User, Products
 from config import ApplicationConfig
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -31,6 +33,26 @@ with app.app_context():
 #         "username": new_user.username
 #     })
 
+
+@app.route("/uploadData", methods=["POST"])
+def get_product_data():
+    productName = request.form["productName"]
+    downloadLink = request.form["url"]
+    image = request.files["image"]
+    filename_split = image.filename.split(".")
+    extention = filename_split[len(filename_split)-1]
+    for_image_path = str(datetime.now().timestamp()).replace(".", "")
+
+    # image.save(f"uploads/{for_image_path}.{extention}")
+    # imagePath = f"uploads/{for_image_path}"
+
+    # new_product = Products(product_name=productName, download_link=downloadLink, image_path=imagePath)
+    # db.session.add(new_product)
+    # db.session.commit()
+    lol = Products.query.all()
+    print(dict(lol))
+    return for_image_path
+
 @app.route("/adminlogin", methods=["POST"])
 def login():
     loginUsername = request.json["username"]
@@ -39,16 +61,20 @@ def login():
     user = User.query.filter_by(username=loginUsername).first()
 
     if user is None:
-        return jsonify({"error": "incorrect username"})
+        return jsonify({"login": "failed", "error": "incorrect username"})
 
     if not bcrypt.check_password_hash(user.password, loginPassword):
-        return jsonify({"error": "incorrect username"})
+        return jsonify({"login": "failed", "error": "incorrect username"})
 
     session["user_id"] = user.id
 
     return jsonify({
         "login": "success"
     })
+
+@app.route("/imagesretrive/<filename>")
+def files(filename):
+    return send_file(f"uploads/{filename}")
 
 @app.route("/admin")
 def admin_page():
--- flask_api/model.py
+++ flask_api/model.py
@@ -11,3 +11,10 @@ class User(db.Model):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid())
     username = db.Column(db.String(300), unique=True)
     password = db.Column(db.Text, nullable=False)
+
+class Products(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid())
+    product_name = db.Column(db.String(100), unique=True, nullable=False)
+    download_link = db.Column(db.Text, nullable=False)
+    image_path = db.Column(db.String(400), nullable=True)
